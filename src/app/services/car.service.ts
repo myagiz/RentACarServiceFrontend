c5cb52d67feb96391cd27811d8b0695cbfc9442d
myagiz <myagiz24@gmail.com>
2021-04-06 12:29:33 +0300

    Get by brandId done.

--- src/app/services/car.service.ts
+++ src/app/services/car.service.ts
@@ -10,12 +10,19 @@ import { ListResponseModel } from '../models/listResponseModel';
 })
 export class CarService {
   
-  apiUrl = 'https://localhost:44396/api/cars/getall';
+  apiUrl = 'https://localhost:44396/api/';
   constructor(private httpClient: HttpClient) { }
 
   getCars():Observable<ListResponseModel<Car>> {
-   return this.httpClient
-    .get<ListResponseModel<Car>>(this.apiUrl);
+  let newPath=this.apiUrl+"cars/getall"
+    return this.httpClient
+    .get<ListResponseModel<Car>>(newPath);
   }
+
+  getCarsByBrandId(brandId:number):Observable<ListResponseModel<Car>> {
+    let newPath=this.apiUrl+"cars/getbybrandid?id="+brandId
+    return this.httpClient
+     .get<ListResponseModel<Car>>(newPath);
+   }
 }
   
